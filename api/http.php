c08c6bbb4b8de2cbc458bdb11e5c88fd8747c761
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-07-12 20:34:51 +0300

    support for downloading any url to a local file

--- api/http.php
+++ api/http.php
@@ -27,13 +27,26 @@ class com_meego_obsconnector_HTTP
         return $this->auth;
     }
 
-    public function get($url)
+    /**
+     * Downloads a url via HTTP GET
+     *
+     * @param string partial or full URL to fetch
+     * @param boolean if true, then the given (partial) URL will be translated
+     *                to a valid OBS API URL
+     *                if false, then the URL will be used as given
+     *
+     * @return string the contents of the file
+     */
+    public function get($url_to_fetch, $apiurl = true)
     {
         $context = stream_context_create(array(
             'http' => array_merge($this->more_options, array('method' => 'GET', 'timeout' => 30)),
         ));
 
-        $url_to_fetch = $this->buildUrl($url);
+        if ($apiurl)
+        {
+            $url_to_fetch = $this->buildUrl($url_to_fetch);
+        }
 
         $content = @file_get_contents($url_to_fetch, false, $context);
 
@@ -85,8 +98,6 @@ class com_meego_obsconnector_HTTP
         return file_get_contents($this->buildUrl($url), false, $context);
     }
 
-
-
     protected function buildUrl($url)
     {
         return $this->protocol.'://'.$this->auth.$this->prefix.$url;
@@ -126,6 +137,39 @@ class com_meego_obsconnector_HTTP
             $this->more_options['request_fulluri'] = true;
         } else {
             trigger_error('"'.$proxy_var_name.'" environment variable is set to the wrong value. expecting http://host:port', E_USER_WARNING);
+        }
+    }
+
+    /**
+     * Downloads any file directly from an URL
+     *
+     * @param string URL to  be downloaded
+     * @param string location the file should be saved to
+     *
+     * @return string location the file it was saved to or null in case of error
+     */
+    public function download($url, $location)
+    {
+        $handle = fopen($location, 'c');
+
+        if ($handle === false)
+        {
+            throw new RuntimeException('Unable to open file for writing: ' . $location);
+            return null;
+        }
+        else
+        {
+            $txt = $this->get($url, false);
+            if ($txt)
+            {
+                $retval = @fwrite($handle, $txt);
+                if ($retval === false)
+                {
+                    throw new RuntimeException('Unable to save to: ' . $location);
+                }
+            }
+            @fclose($handle);
+            return $location;
         }
     }
 }
